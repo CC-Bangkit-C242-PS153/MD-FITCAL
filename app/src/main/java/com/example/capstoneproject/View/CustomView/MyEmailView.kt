f7573f147f66ca025c7388cf3c3f88f5419b040b
Sedika <sedikasipayung5@gmail.com>
2024-11-20 01:18:26 +0700

    Menambahkan layout login serta custom view untuk InputText email dan password

--- app/src/main/java/com/example/capstoneproject/View/CustomView/MyEmailView.kt
+++ app/src/main/java/com/example/capstoneproject/View/CustomView/MyEmailView.kt
@@ -1,4 +1,58 @@
 package com.example.capstoneproject.View.CustomView
 
-class MyEmailView {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class MyEmailView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) :AppCompatEditText(context,attrs), View.OnTouchListener {
+    init {
+        setOnTouchListener(this)
+        addTextChangedListener(object :TextWatcher{
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null && !Patterns.EMAIL_ADDRESS.matcher(s).matches()){
+                    setError("Invalid email format",null)
+                }else{
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Enter your email"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ){
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText)
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/example/capstoneproject/View/Main/MainActivity.kt
+++ app/src/main/java/com/example/capstoneproject/View/Main/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.example.capstoneproject
+package com.example.capstoneproject.View.Main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.capstoneproject.R
+import com.example.capstoneproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
