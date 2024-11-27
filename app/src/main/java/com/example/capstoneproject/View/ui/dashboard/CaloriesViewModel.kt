51d3e1bfeed4607b9d4ebfa149d5064db634765e
Sedika <sedikasipayung5@gmail.com>
2024-11-27 22:30:10 +0700

    Pembuatan bottom navigation

---
+++ app/src/main/java/com/example/capstoneproject/View/ui/dashboard/CaloriesViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.capstoneproject.View.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CaloriesViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
